6737e7e7bca0fdf8ba45d6daee52cd4e1b089eb8
Shaxadhere <Shaxad.here@gmail.com>
2021-02-12 11:38:16 +0500

    client

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,9 @@
-export default (posts = [], action)  = {
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+export default (posts = [], action)  => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
             // break;
         case 'CREATE':
             return posts;
